3dccbbaa7175fd072c208ea0f78a523717e32666
silverian <dyrus1107@gmail.com>
2024-10-29 14:45:15 +0700

    "Added Swagger"

--- apps/server/src/notes/dto/update-note.dto.ts
+++ apps/server/src/notes/dto/update-note.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import {
   IsNotEmpty,
@@ -6,35 +7,73 @@ import {
   ValidateNested,
 } from 'class-validator';
 
+class NoteBlockDto {
+  @ApiProperty({
+    description: 'Id of a note block',
+    example: '671fc56910a44757a43d86b2',
+    type: String,
+  })
+  @IsNotEmpty()
+  id: string;
+
+  @ApiProperty({
+    description: 'Type of a note block',
+    example: 'paragraph',
+    type: String,
+  })
+  @IsNotEmpty()
+  type: string;
+
+  @ApiProperty({
+    description: 'Data of a note block',
+    type: Object,
+  })
+  @IsNotEmpty()
+  data: any;
+
+  @ApiProperty({
+    description: 'Tunes of a note block',
+    type: Object,
+  })
+  @IsOptional()
+  tunes?: any;
+}
+
 class UpdateNoteBodyDto {
+  @ApiProperty({
+    description: 'Time of the note',
+    example: 12345678,
+    type: Number,
+  })
   @IsNotEmpty()
   time: number;
 
+  @ApiProperty({
+    description: 'Blocks of the note',
+    type: [NoteBlockDto],
+  })
   @IsNotEmpty()
   @ValidateNested({ each: true })
   @Type(() => NoteBlockDto)
   blocks: NoteBlockDto[];
 }
 
-class NoteBlockDto {
-  @IsNotEmpty()
-  id: string;
-
-  @IsNotEmpty()
-  type: string;
-
-  @IsNotEmpty()
-  data: any;
-
-  @IsOptional()
-  tunes?: any;
-}
-
 export class UpdateNoteDto {
+  @ApiProperty({
+    description: 'Name of the note',
+    required: false,
+    example: 'My note',
+    type: String,
+  })
   @IsOptional()
   @Length(1, 100)
   name?: string;
 
+  @ApiProperty({
+    description: 'Body of the note',
+    required: false,
+    type: UpdateNoteBodyDto,
+  })
   @IsOptional()
   @ValidateNested()
   @Type(() => UpdateNoteBodyDto)
--- apps/server/src/notes/notes.controller.ts
+++ apps/server/src/notes/notes.controller.ts
@@ -9,22 +9,39 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
-import { NotesService } from './notes.service';
-import { CreateNoteDto } from './dto/create-note.dto';
-import { Note } from './models/note.models';
+import {
+  ApiBody,
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { SortOptions } from 'src/utils/dtos/options.dto';
+import { CreateNoteDto } from './dto/create-note.dto';
 import { UpdateNoteDto } from './dto/update-note.dto';
+import { Note } from './models/note.models';
+import { NotesService } from './notes.service';
 
+@ApiTags('Notes')
 @Controller('notes')
 export class NotesController {
   constructor(private readonly notesService: NotesService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Create a new note' })
+  @ApiResponse({
+    status: 201,
+    description: 'The note has been successfully created.',
+  })
+  @ApiResponse({ status: 400, description: 'Bad Request.' })
+  @ApiBody({ type: CreateNoteDto })
   async create(@Body() noteData: CreateNoteDto) {
     return this.notesService.create(noteData);
   }
 
   @Get()
+  @ApiOperation({ summary: 'Get all notes' })
+  @ApiResponse({ status: 200, description: 'Return all notes.' })
   async findAll(): Promise<Note[]> {
     try {
       const notes = await this.notesService.findAll();
@@ -40,6 +57,13 @@ export class NotesController {
   }
 
   @Get(':ownerId/notes')
+  @ApiOperation({ summary: 'Get notes by owner ID' })
+  @ApiParam({ name: 'ownerId', required: true })
+  @ApiResponse({
+    status: 200,
+    description: 'Return notes for specified owner.',
+  })
+  @ApiResponse({ status: 400, description: 'Bad Request.' })
   async findByOwner(
     @Param('ownerId') ownerId: string,
     @Query() sortOptions: SortOptions,
@@ -58,11 +82,23 @@ export class NotesController {
   }
 
   @Get(':noteId')
+  @ApiOperation({ summary: 'Get a note by ID' })
+  @ApiParam({ name: 'noteId', required: true })
+  @ApiResponse({ status: 200, description: 'Return the note.' })
+  @ApiResponse({ status: 404, description: 'Note not found.' })
   async findById(@Param('noteId') noteId: string) {
     return this.notesService.findById(noteId);
   }
 
   @Patch(':noteId')
+  @ApiOperation({ summary: 'Update a note by ID' })
+  @ApiParam({ name: 'noteId', required: true })
+  @ApiResponse({
+    status: 200,
+    description: 'The note has been successfully updated.',
+  })
+  @ApiResponse({ status: 404, description: 'Note not found.' })
+  @ApiBody({ type: UpdateNoteDto })
   async updateById(
     @Param('noteId') noteId: string,
     @Body() updateData: UpdateNoteDto,
@@ -71,16 +107,37 @@ export class NotesController {
   }
 
   @Patch(':noteId/archive')
+  @ApiOperation({ summary: 'Archive a note by ID' })
+  @ApiParam({ name: 'noteId', required: true })
+  @ApiResponse({
+    status: 200,
+    description: 'The note has been successfully archived.',
+  })
+  @ApiResponse({ status: 404, description: 'Note not found.' })
   async archiveById(@Param('noteId') noteId: string) {
     return this.notesService.archiveById(noteId);
   }
 
   @Patch(':noteId/restore')
+  @ApiOperation({ summary: 'Restore a note by ID' })
+  @ApiParam({ name: 'noteId', required: true })
+  @ApiResponse({
+    status: 200,
+    description: 'The note has been successfully restored.',
+  })
+  @ApiResponse({ status: 404, description: 'Note not found.' })
   async restoreById(@Param('noteId') noteId: string) {
     return this.notesService.restoreById(noteId);
   }
 
   @Patch(':noteId/delete')
+  @ApiOperation({ summary: 'Delete a note by ID' })
+  @ApiParam({ name: 'noteId', required: true })
+  @ApiResponse({
+    status: 200,
+    description: 'The note has been successfully deleted.',
+  })
+  @ApiResponse({ status: 404, description: 'Note not found.' })
   async deleteById(@Param('noteId') noteId: string) {
     return this.notesService.deleteById(noteId);
   }
